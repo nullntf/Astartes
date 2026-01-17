ab25afcd78be7b7e5f3fca2e0f2d5c1059f006f0
nullntf <223046824+nullntf@users.noreply.github.com>
2026-01-17 14:08:36 -0600

    fix: Limpiar cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Clear any existing cache first
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
 # Cache configuration
 php artisan config:cache
 php artisan route:cache
